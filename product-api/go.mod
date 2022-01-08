caca9024a377d455c7bf7c3440a965fb606e1969
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-09 00:20:35 +0100

    start using gorrilla framework

--- product-api/go.mod
+++ product-api/go.mod
@@ -1,3 +1,5 @@
 module github.com/mu-majid/microservices-go/product-api
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- product-api/main.go
+++ product-api/main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/mu-majid/microservices-go/product-api/handlers"
 )
 
@@ -22,8 +23,20 @@ func main() {
 	ph := handlers.NewProducts(l)
 
 	// create a new serve mux and register the handlers
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	// sm.Handle("/", ph)
 
 	// create a new server
 	s := http.Server{
