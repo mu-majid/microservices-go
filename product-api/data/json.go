4ffe0c46a4c14afbbfba68394ec203b636067e30
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-09 18:02:32 +0100

    refactor data layer

---
+++ product-api/data/json.go
@@ -0,0 +1,26 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// ToJSON serializes the given interface into a string based JSON format
+// ToJSON serializes the contents of the collection to JSON
+// NewEncoder provides better performance than json.Unmarshal as it does not
+// have to buffer the output into an in memory slice of bytes
+// this reduces allocations and the overheads of the service
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
+func ToJSON(i interface{}, w io.Writer) error {
+	e := json.NewEncoder(w)
+
+	return e.Encode(i)
+}
+
+// FromJSON deserializes the object from JSON string
+// in an io.Reader to the given interface
+func FromJSON(i interface{}, r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(i)
+}
--- product-api/go.mod
+++ product-api/go.mod
@@ -3,8 +3,10 @@ module github.com/mu-majid/microservices-go/product-api
 go 1.16
 
 require (
+	github.com/go-openapi/runtime v0.21.0
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/stretchr/testify v1.7.0
 )
