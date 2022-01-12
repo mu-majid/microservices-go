61c7b1cd3294c36e566193fc4ab90705102035dc
Muhammad Majid <mmajidkamel@gmail.com>
2022-01-12 23:46:14 +0100

    Added second microservice for file handling (uploading prod imgs)

---
+++ product-images/go.mod
@@ -0,0 +1,3 @@
+module github.com/mu-majid/microservices-go/product-images
+
+go 1.13
---
+++ product-images/handlers/files.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+	"github.com/mu-majid/microservices-go/product-images/files"
+)
+
+// Files is a handler for reading and writing files
+type Files struct {
+	log   hclog.Logger
+	store files.Storage
+}
+
+// NewFiles creates a new File handler
+func NewFiles(s files.Storage, l hclog.Logger) *Files {
+	return &Files{store: s, log: l}
+}
+
+// ServeHTTP implements the http.Handler interface
+func (f *Files) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fn := vars["filename"]
+
+	f.log.Info("Handle POST", "id", id, "filename", fn)
+
+	// no need to check for invalid id or filename as the mux router will not send requests
+	// here unless they have the correct parameters
+
+	f.saveFile(id, fn, rw, r)
+}
+
+func (f *Files) invalidURI(uri string, rw http.ResponseWriter) {
+	f.log.Error("Invalid path", "path", uri)
+	http.Error(rw, "Invalid file path should be in the format: /[id]/[filepath]", http.StatusBadRequest)
+}
+
+// saveFile saves the contents of the request to a file
+func (f *Files) saveFile(id, path string, rw http.ResponseWriter, r *http.Request) {
+	f.log.Info("Save file for product", "id", id, "path", path)
+
+	fp := filepath.Join(id, path)
+	err := f.store.Save(fp, r.Body)
+	if err != nil {
+		f.log.Error("Unable to save file", "error", err)
+		http.Error(rw, "Unable to save file", http.StatusInternalServerError)
+	}
+}
